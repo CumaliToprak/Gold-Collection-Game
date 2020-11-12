548def82ad04e5ed58957ac5f50573ab50bd0e94
cumali toprak <cumalitoprakk@gmail.com>
2020-11-12 16:53:39 +0300

    initial commit

---
+++ Kordinat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altın_Toplama_Oyunu
+{
+    public class Kordinat
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ StartGame.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altın_Toplama_Oyunu
+{
+    class StartGame
+    {
+        frmCreateBoard createBoard;
+        public StartGame()
+        {
+
+        }
+
+        internal void createGameBoard(int boardX, int boardY, int altinOrani)
+        {
+            List<Kordinat> altinKonumlari = randomKareleriBul(boardX, boardY, altinOrani);
+
+            createBoard = new frmCreateBoard(boardX, boardY, altinOrani, altinKonumlari);
+            createBoard.create();
+
+        }
+
+        public List<Kordinat> randomKareleriBul(int boardX, int boardY, int altinOrani)
+        {
+            int kareSayisi = boardX * boardY;
+            int altınSayisi = (kareSayisi * altinOrani) / 100;
+
+            Random rnd = new Random();
+
+            List<Kordinat> randomList = new List<Kordinat>();
+
+            int x,y;
+            int temp = 0;
+            Console.WriteLine("Before: {0}",altınSayisi);
+            while (temp < altınSayisi)
+            {
+                x = rnd.Next(0, boardX);
+                y = rnd.Next(0, boardY);
+                if (randomList.FindIndex(p=> p.X==x && p.Y==y) < 0)
+                {
+                    randomList.Add(new Kordinat { X = x, Y = y });
+                    temp++;
+                }
+                   
+            }
+
+            Console.WriteLine(randomList.Count + ":" );
+            foreach (var kordinat in randomList)
+            {
+                Console.WriteLine(kordinat.X + " , " + kordinat.Y + "    ");
+            }
+                
+            return randomList;
+        }
+
+
+        
+    
+
+       
+
+
+
+
+    }
+}
+
